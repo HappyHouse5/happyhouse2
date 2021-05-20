eac50c8e6f4a94df4eb367d7b88c974a5f455617
smy999 <210113yynim@naver.com>
2021-05-20 14:43:02 +0900

    member form color

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import { Popover } from 'bootstrap';
+import Axios from 'axios';
+// import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
+Vue.use(Popover);
+Vue.prototype.$http = Axios;
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+  // {
+  //   path: "/",
+  //   name: "Home",
+  //   component: Home,
+  // },
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  // },
 ];
 
 const router = new VueRouter({
