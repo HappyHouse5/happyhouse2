2e80802cc04f0f467ae538509535f69f7262f85d
smy999 <210113yynim@naver.com>
2021-05-21 20:06:32 +0900

    login modal

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
 <!-- ======= Header ======= -->
     <header id="header" class="fixed-top d-flex align-items-center">
       <div class="container d-flex justify-content-between">
@@ -11,6 +12,8 @@
         <nav id="navbar" class="navbar">
           <ul>
             <a class="nav-link" style="cursor:pointer" v-on:click="$router.push({name: 'Home'}).catch(()=>{});">Home</a>
+            <li><a class="nav-link scrollto" v-on:click="login">Login</a></li>
+            <!-- <li><a class="nav-link scrollto" v-on:click="$router.push({name: 'Logout'}).catch(()=>{});">Logout</a></li> -->
             <li><a class="nav-link scrollto" v-on:click="$router.push({name: 'Join'}).catch(()=>{});">Join</a></li>
             <li><a class="nav-link scrollto" v-on:click="$router.push({name: 'Member', params: {type: 'info'}}).catch(()=>{});">MyPage</a></li>
             <li><a class="nav-link scrollto" v-on:click="$router.push({name: 'House'}).catch(()=>{});">House</a></li>
@@ -22,11 +25,37 @@
       </div>
     </header>
     <!-- End Header -->
+
+    <login-modal v-bind:loginModal=loginModal></login-modal>
+  </div>
 </template>
 
+
+
 <script>
+import LoginModal from '@/components/modals/LoginModal.vue';
+import {Modal} from 'bootstrap';
+
 export default {
     name: "NavBar",
+    components: {
+      LoginModal,
+    },
+    data() {
+      return {
+        showModal: false,
+        loginModal: null,
+      }
+    },
+    mounted() {
+      this.loginModal = new Modal(document.querySelector("#loginModal"));
+    },
+    methods: {
+      login() {
+        console.log("login modal");
+        this.loginModal.show();
+      }
+    }
 }
 </script>
 
---
+++ src/components/modals/LoginModal.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="modal" tabindex="-1" id="loginModal">
+<div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+        </div>
+        <div class="modal-body">
+          <form class="form-signin">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <label for="inputEmail" class="sr-only">User ID</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Email address"
+              required
+            />
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input
+              type="password"
+              id="userpwd"
+              class="form-control"
+              placeholder="Password"
+              required
+            />
+            <div class="checkbox mb-3">
+              <label> <input type="checkbox" value="remember-me" /> Remember me </label>
+            </div>
+            
+          </form>
+        </div>
+        <div class="modal-footer">
+            <button
+              id="btnLogin"
+              class="btn btn-primary"
+              v-on:click="loginSubmit"
+              data-dismiss="modal"
+              >Sign in</button
+            >
+          <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="closeModal"
+            >Close</button
+          >
+        </div>
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoginModal",
+    props: ['loginModal'],
+    methods: {
+        loginSubmit() {
+            console.log("login submit");
+        },
+        closeModal() {
+            this.loginModal.hide();
+        }
+    }
+}
+</script>
