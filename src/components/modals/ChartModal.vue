e8261b28257f1f500a14c1d93fc9edd49c4fe2a2
smy999 <210113yynim@naver.com>
2021-05-25 18:04:30 +0900

    pagination

--- src/components/modals/ChartModal.vue
+++ src/components/modals/ChartModal.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="modal" tabindex="-1" id="chartModal">
-<div class="modal-dialog">
+<div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">House Chart</h5>
         </div>
         <div class="modal-body">
-          <chart-vue :chartData="chartData"></chart-vue>
+          <chart-vue style="height:400px;" :chartData="chartData"></chart-vue>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="closeModal"
